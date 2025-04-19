9867f3b58ac793b5ba1c59d8acf32fe028d9c4bc
DominguezEmanuel <dmanu401@gmail.com>
2025-04-19 12:17:12 -0300

    Página de entrenadores terminada

--- js/efecto-tarjetas.js
+++ js/efecto-tarjetas.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
-  $(document).ready(function () {
-    $(".flip-card").on("click", function () {
-      $(this).toggleClass("flipped");
-    });
+  $(".progress-bar").each(function () {
+    var $this = $(this);
+    var value = $this.attr("aria-valuenow");
+    $this.animate(
+      {
+        width: value + "%",
+      },
+      1000
+    );
+  });
+
+  // Estrellas de rating interactivas
+  $(".rating input").on("change", function () {
+    var selected = $(this).val();
+    $(this).parent().attr("data-selected", selected);
+    console.log("Puntaje seleccionado: " + selected);
   });
 });
